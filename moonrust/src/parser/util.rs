0fab82ad22d5d70d8193f9720880a3c4a52c8ac4
matthewdellaneve <mjdellaneve@gmail.com>
2023-04-01 16:46:48 -0400

    More More Expression Parsing - Updated parse_float to correctly reject inputs with only an int part - Made parse_dot_dot_dot ignore whitespace - Added parse_table_constructor, parse_binop_exp, and parse_unop_exp

--- moonrust/src/parser/util.rs
+++ moonrust/src/parser/util.rs
@@ -4,11 +4,11 @@ use nom::{
         complete::{is_not, take_while_m_n},
         streaming::tag,
     },
-    character::complete::{alpha1, alphanumeric1, char, multispace0, multispace1},
-    combinator::{map, map_opt, map_res, recognize, value, verify},
+    character::complete::{alpha1, alphanumeric1, char, multispace0, multispace1, one_of},
+    combinator::{map, map_opt, map_res, opt, recognize, value, verify},
     error::{FromExternalError, ParseError},
-    multi::{fold_many0, many0_count},
-    sequence::{delimited, pair, preceded},
+    multi::{fold_many0, many0, many0_count, many1},
+    sequence::{delimited, pair, preceded, terminated, tuple},
     IResult,
 };
 
@@ -165,6 +165,42 @@ pub fn identifier(input: &str) -> IResult<&str, &str> {
             alt((alpha1, tag("_"))),
             many0_count(alt((alphanumeric1, tag("_")))),
         )),
-        |result: &str| KEYWORDS.contains(&result),
+        |result: &str| !KEYWORDS.contains(&result),
     )(input)
+}
+
+pub fn float(input: &str) -> IResult<&str, &str> {
+    alt((
+        // Case one: .42
+        recognize(tuple((
+            char('.'),
+            decimal,
+            opt(tuple((one_of("eE"), opt(one_of("+-")), decimal))),
+        ))), // Case two: 42e42 and 42.42e42
+        recognize(tuple((
+            decimal,
+            opt(preceded(char('.'), decimal)),
+            one_of("eE"),
+            opt(one_of("+-")),
+            decimal,
+        ))), // Case three: 42. and 42.42
+        recognize(tuple((decimal, char('.'), opt(decimal)))),
+    ))(input)
+}
+
+fn decimal(input: &str) -> IResult<&str, &str> {
+    recognize(many1(terminated(one_of("0123456789"), many0(char('_')))))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo1() {
+        let input = "1.45678";
+        let result = float(input);
+
+        assert_eq!(result, Ok(("", "1.45678")));
+    }
 }
