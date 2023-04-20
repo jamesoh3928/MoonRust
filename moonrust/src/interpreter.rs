545fb3c0cc0a76429e91b55ca885969ad304a962
James Oh <jo9347@cs.rit.edu>
2023-04-20 15:46:08 -0400

    DoBlock, While, Repeat semantics implemented. Also some clean up in test

--- moonrust/src/interpreter.rs
+++ moonrust/src/interpreter.rs
@@ -61,19 +61,38 @@ impl AST {
 }
 
 impl Block {
-    fn exec<'a, 'b>(&'a self, env: &'b mut Env<'a>) -> Result<Vec<LuaValue<'a>>, ASTExecError> {
+    fn exec<'a, 'b>(
+        &'a self,
+        env: &'b mut Env<'a>,
+    ) -> Result<Option<Vec<LuaValue<'a>>>, ASTExecError> {
+        let return_vals = self.exec_without_pop(env)?;
+        // Remove environment when exiting a scope
+        env.pop_local_env();
+
+        Ok(return_vals)
+    }
+
+    fn exec_without_pop<'a, 'b>(
+        &'a self,
+        env: &'b mut Env<'a>,
+    ) -> Result<Option<Vec<LuaValue<'a>>>, ASTExecError> {
         // Extend environment when entering a new scope
         env.extend_local_env();
 
         // Execute each statement
         for statement in &self.statements {
-            statement.exec(env)?;
+            let return_vals = statement.exec(env)?;
+            if return_vals.is_none() {
+                // Break statement
+                return Ok(None);
+            }
         }
 
         // Optional return statement
         let explist = match &self.return_stat {
             Some(explist) => explist,
-            None => return Ok(vec![]),
+            // Returning empty vector means there was no return statement
+            None => return Ok(Some(vec![])),
         };
 
         let mut return_vals = vec![LuaValue::new(LuaVal::LuaNil); explist.len()];
@@ -83,15 +102,12 @@ impl Block {
             i += 1;
         }
 
-        // Remove environment when exiting a scope
-        env.pop_local_env();
-
-        Ok(return_vals)
+        Ok(Some(return_vals))
     }
 }
 
 impl Statement {
-    fn exec<'a, 'b>(&'a self, env: &'b mut Env<'a>) -> Result<(), ASTExecError> {
+    fn exec<'a, 'b>(&'a self, env: &'b mut Env<'a>) -> Result<Option<Vec<LuaValue>>, ASTExecError> {
         match self {
             Statement::Semicolon => {
                 // Do nothing
@@ -133,19 +149,65 @@ impl Statement {
                 funcall.exec(env)?;
             }
             Statement::Break => {
-                // TODO: return some value to indicate break
-                unimplemented!()
+                // Terminates the execution of a while, repeat, or for loop
+                return Ok(None);
             }
             Statement::DoBlock(block) => {
-                unimplemented!()
+                // TODO: add test for this
+                match block.exec(env) {
+                    Ok(val) => match val {
+                        Some(val) => {
+                            return Ok(Some(val));
+                        }
+                        None => {
+                            panic!("Break statement can only be used inside a while, repeat, or for loop");
+                        }
+                    },
+                    Err(err) => {
+                        return Err(err);
+                    }
+                }
             }
             Statement::While((exp, block)) => {
-                // TODO: have to do it so that we don't lose exp and block....
                 // Execute block until exp returns false
-                unimplemented!()
+                // Local variables are lost in each iteration
+                // TODO: handle break statement
+                while exp.eval(env)?.is_true() {
+                    let return_vals = block.exec(env)?;
+                    match return_vals {
+                        Some(return_vals) => {
+                            if !return_vals.is_empty() {
+                                return Ok(Some(return_vals));
+                            }
+                        }
+                        None => {
+                            // Break statement (exiting loop now so return empty vector)
+                            return Ok(Some(vec![]));
+                        }
+                    }
+                }
             }
             Statement::Repeat((block, exp)) => {
-                unimplemented!()
+                // condition can refer to local variables declared inside the loop block
+                loop {
+                    let return_vals = block.exec_without_pop(env)?;
+                    match return_vals {
+                        Some(return_vals) => {
+                            if !return_vals.is_empty() {
+                                return Ok(Some(return_vals));
+                            }
+                        }
+                        None => {
+                            // Break statement (exiting loop now so return empty vector)
+                            return Ok(Some(vec![]));
+                        }
+                    }
+                    if exp.eval(env)?.is_true() {
+                        break;
+                    }
+                    // Pop local environment before next iteration
+                    env.pop_local_env();
+                }
             }
             Statement::If((exp, block, elseifs, elseblock)) => {
                 let condition = exp.eval(env)?;
@@ -157,7 +219,7 @@ impl Statement {
                         let condition = exp.eval(env)?;
                         if condition.is_true() {
                             block.exec(env)?;
-                            return Ok(());
+                            return Ok(Some(vec![]));
                         }
                     }
                     if let Some(elseblock) = elseblock {
@@ -179,7 +241,7 @@ impl Statement {
             }
         };
 
-        Ok(())
+        Ok(Some(vec![]))
     }
 }
 
@@ -194,7 +256,7 @@ impl Expression {
                 Numeral::Float(f) => LuaValue::new(LuaVal::LuaNum(f.to_be_bytes())),
             },
             Expression::LiteralString(s) => LuaValue::new(LuaVal::LuaString(s.clone())),
-            // TODO: DotDotDot?
+            // TODO: DotDotDot? maybe skip it for now
             Expression::DotDotDot => unimplemented!(),
             Expression::FunctionDef((par_list, block)) => {
                 LuaValue::new(LuaVal::Function(LuaFunction { par_list, block }))
@@ -297,7 +359,12 @@ impl FunctionCall {
 
                         // Remove arguments from the environment
                         env.pop_local_env();
-                        Ok(result)
+                        match result {
+                            Some(vals) => Ok(vals),
+                            None => panic!(
+                                "Break statement can be only used in while, repeat, or for loop"
+                            ),
+                        }
                     }
                     _ => {
                         return Err(ASTExecError(format!(
@@ -326,6 +393,32 @@ impl Display for ASTExecError {
 mod tests {
     use super::*;
 
+    // Helper functions
+    fn var_exp(name: &str) -> Expression {
+        Expression::PrefixExp(Box::new(PrefixExp::Var(Var::NameVar(name.to_string()))))
+    }
+    fn lua_integer<'a>(n: i64) -> LuaValue<'a> {
+        LuaValue::new(LuaVal::LuaNum(n.to_be_bytes()))
+    }
+    fn lua_float<'a>(n: f64) -> LuaValue<'a> {
+        LuaValue::new(LuaVal::LuaNum(n.to_be_bytes()))
+    }
+    fn lua_nil<'a>() -> LuaValue<'a> {
+        LuaValue::new(LuaVal::LuaNil)
+    }
+    fn lua_false<'a>() -> LuaValue<'a> {
+        LuaValue::new(LuaVal::LuaBool(false))
+    }
+    fn lua_true<'a>() -> LuaValue<'a> {
+        LuaValue::new(LuaVal::LuaBool(true))
+    }
+    fn lua_string<'a>(s: &str) -> LuaValue<'a> {
+        LuaValue::new(LuaVal::LuaString(s.to_string()))
+    }
+    fn lua_function<'a>(par_list: &'a ParList, block: &'a Block) -> LuaValue<'a> {
+        LuaValue::new(LuaVal::Function(LuaFunction { par_list, block }))
+    }
+
     // TODO split into multiple tests
     #[test]
     fn test_eval_exp_nil() {
@@ -333,7 +426,7 @@ mod tests {
 
         // Nil
         let exp_nil = Expression::Nil;
-        assert_eq!(exp_nil.eval(&mut env), Ok(LuaValue::new(LuaVal::LuaNil)));
+        assert_eq!(exp_nil.eval(&mut env), Ok(lua_nil()));
     }
 
     #[test]
@@ -343,14 +436,8 @@ mod tests {
         // Boolean
         let exp_false = Expression::False;
         let exp_true = Expression::True;
-        assert_eq!(
-            exp_false.eval(&mut env),
-            Ok(LuaValue::new(LuaVal::LuaBool(false)))
-        );
-        assert_eq!(
-            exp_true.eval(&mut env),
-            Ok(LuaValue::new(LuaVal::LuaBool(true)))
-        );
+        assert_eq!(exp_false.eval(&mut env), Ok(lua_false()));
+        assert_eq!(exp_true.eval(&mut env), Ok(lua_true()));
     }
 
     #[test]
@@ -360,10 +447,7 @@ mod tests {
         // Integer
         let num: i64 = 10;
         let exp_int = Expression::Numeral(Numeral::Integer(num));
-        assert_eq!(
-            exp_int.eval(&mut env),
-            Ok(LuaValue::new(LuaVal::LuaNum(num.to_be_bytes())))
-        );
+        assert_eq!(exp_int.eval(&mut env), Ok(lua_integer(num)));
     }
 
     #[test]
@@ -373,10 +457,7 @@ mod tests {
         // Float
         let num: f64 = 10.04;
         let exp_float = Expression::Numeral(Numeral::Float(num));
-        assert_eq!(
-            exp_float.eval(&mut env),
-            Ok(LuaValue::new(LuaVal::LuaNum(num.to_be_bytes())))
-        );
+        assert_eq!(exp_float.eval(&mut env), Ok(lua_float(num)));
     }
 
     #[test]
@@ -385,10 +466,7 @@ mod tests {
 
         // String
         let exp_str = Expression::LiteralString("Hello World!".to_string());
-        assert_eq!(
-            exp_str.eval(&mut env),
-            Ok(LuaValue::new(LuaVal::LuaString("Hello World!".to_string())))
-        );
+        assert_eq!(exp_str.eval(&mut env), Ok(lua_string("Hello World!")));
     }
 
     #[test]
@@ -402,15 +480,12 @@ mod tests {
             statements: vec![],
             return_stat: None,
         };
-        let exp_func_def = Expression::FunctionDef((par_list, block));
+        let exp_func_def = Expression::FunctionDef((par_list.clone(), block.clone()));
         assert_eq!(
             exp_func_def.eval(&mut env),
             Ok(LuaValue::new(LuaVal::Function(LuaFunction {
-                par_list: &ParList(vec![String::from("test")], false),
-                block: &Block {
-                    statements: vec![],
-                    return_stat: None,
-                }
+                par_list: &par_list,
+                block: &block
             })))
         );
     }
@@ -427,9 +502,7 @@ mod tests {
             Expression::Numeral(Numeral::Integer(20)),
         ];
         let stat = Statement::Assignment((varlist, explist));
-        let return_stat = Some(vec![Expression::PrefixExp(Box::new(PrefixExp::Var(
-            Var::NameVar("test".to_string()),
-        )))]);
+        let return_stat = Some(vec![var_exp("test")]);
 
         let par_list = ParList(vec![String::from("test")], false);
         let block = Block {
@@ -437,23 +510,18 @@ mod tests {
             return_stat: return_stat,
         };
 
-        env.insert_global(
-            String::from("f"),
-            LuaValue::new(LuaVal::Function(LuaFunction {
-                par_list: &par_list,
-                block: &block,
-            })),
-        );
-        let func_prefix = PrefixExp::Var(Var::NameVar("f".to_string()));
+        env.insert_global(String::from("f"), lua_function(&par_list, &block));
         let args = Args::ExpList(vec![Expression::Numeral(Numeral::Integer(100))]);
-        let func_call = FunctionCall::Standard((Box::new(func_prefix), args));
+        let func_call = FunctionCall::Standard((
+            Box::new(PrefixExp::Var(Var::NameVar("f".to_string()))),
+            args,
+        ));
         let exp = PrefixExp::FunctionCall(func_call);
-        let hundered: i64 = 100;
 
         // f(100) executes a = 30, b = 20, return test
         assert_eq!(
             exp.eval(&mut env),
-            Ok(LuaValue::new(LuaVal::LuaNum(hundered.to_be_bytes())))
+            Ok(lua_integer(100))
         );
     }
 
@@ -476,7 +544,7 @@ mod tests {
             Expression::Numeral(Numeral::Integer(10)),
         ];
         let stat = Statement::Assignment((varlist, explist));
-        assert_eq!(stat.exec(&mut env), Ok(()));
+        assert_eq!(stat.exec(&mut env), Ok(Some(vec![])));
         assert_eq!(*env.get("a").unwrap().0, LuaVal::LuaNum(a.to_be_bytes()));
         assert_eq!(*env.get("b").unwrap().0, LuaVal::LuaNum(b.to_be_bytes()));
 
@@ -493,7 +561,7 @@ mod tests {
             Expression::Numeral(Numeral::Integer(20)),
         ];
         let stat = Statement::Assignment((varlist, explist));
-        assert_eq!(stat.exec(&mut env), Ok(()));
+        assert_eq!(stat.exec(&mut env), Ok(Some(vec![])));
         assert_eq!(*env.get("a").unwrap().0, LuaVal::LuaNum(a.to_be_bytes()));
         assert_eq!(*env.get("b").unwrap().0, LuaVal::LuaNum(b.to_be_bytes()));
         assert_eq!(*env.get("c").unwrap().0, LuaVal::LuaNil);
@@ -508,7 +576,7 @@ mod tests {
             Expression::Numeral(Numeral::Integer(10)),
         ];
         let stat = Statement::Assignment((varlist, explist));
-        assert_eq!(stat.exec(&mut env), Ok(()));
+        assert_eq!(stat.exec(&mut env), Ok(Some(vec![])));
         assert_eq!(*env.get("a").unwrap().0, LuaVal::LuaNum(a.to_be_bytes()));
         assert_eq!(*env.get("b").unwrap().0, LuaVal::LuaNum(b.to_be_bytes()));
     }
@@ -524,7 +592,7 @@ mod tests {
         let a: i64 = 10;
         env.insert_global(
             "a".to_string(),
-            LuaValue::new(LuaVal::LuaNum(a.to_be_bytes())),
+            lua_integer(a),
         );
 
         // Simple function with side effect (a = 10.04)
@@ -541,10 +609,7 @@ mod tests {
         let par_list = ParList(vec![], false);
         env.insert_global(
             "f".to_string(),
-            LuaValue::new(LuaVal::Function(LuaFunction {
-                par_list: &par_list,
-                block: &block,
-            })),
+            lua_function(&par_list, &block),
         );
         // Function call statement
         let func_prefix = PrefixExp::Var(Var::NameVar("f".to_string()));
@@ -552,10 +617,10 @@ mod tests {
         let func_call = FunctionCall::Standard((Box::new(func_prefix), args));
         let func_call_stat = Statement::FunctionCall(func_call);
 
-        assert_eq!(func_call_stat.exec(&mut env), Ok(()));
+        assert_eq!(func_call_stat.exec(&mut env), Ok(Some(vec![])));
         assert_eq!(
             env.get("a"),
-            Some(&LuaValue::new(LuaVal::LuaNum(num.to_be_bytes())))
+            Some(&lua_float(num))
         );
     }
 
@@ -571,10 +636,10 @@ mod tests {
             return_stat: None,
         };
         let if_stat = Statement::If((condition, block, vec![], None));
-        assert_eq!(if_stat.exec(&mut env), Ok(()));
+        assert_eq!(if_stat.exec(&mut env), Ok(Some(vec![])));
         assert_eq!(
             env.get("a"),
-            Some(&LuaValue::new(LuaVal::LuaNum(10i64.to_be_bytes())))
+            Some(&lua_integer(10))
         );
     }
 
@@ -597,10 +662,10 @@ mod tests {
             return_stat: None,
         };
         let if_stat = Statement::If((condition, block, vec![], Some(else_block)));
-        assert_eq!(if_stat.exec(&mut env), Ok(()));
+        assert_eq!(if_stat.exec(&mut env), Ok(Some(vec![])));
         assert_eq!(
             env.get("a"),
-            Some(&LuaValue::new(LuaVal::LuaNum(20i64.to_be_bytes())))
+            Some(&lua_integer(20))
         );
     }
 
@@ -633,10 +698,87 @@ mod tests {
             return_stat: None,
         };
         let if_stat = Statement::If((condition, block, else_ifs, Some(else_block)));
-        assert_eq!(if_stat.exec(&mut env), Ok(()));
+        assert_eq!(if_stat.exec(&mut env), Ok(Some(vec![])));
         assert_eq!(
             env.get("a"),
-            Some(&LuaValue::new(LuaVal::LuaNum(20i64.to_be_bytes())))
+            Some(&lua_integer(20))
         );
+    }
+
+    #[test]
+    fn test_exec_stat_doblock() {
+        let mut env = Env::new();
+        let block = Block {
+            statements: vec![Statement::Assignment((
+                vec![Var::NameVar("a".to_string())],
+                vec![Expression::Numeral(Numeral::Integer(10))],
+            ))],
+            return_stat: Some(vec![Expression::PrefixExp(Box::new(PrefixExp::Var(
+                Var::NameVar("a".to_string()),
+            )))]),
+        };
+        let do_block = Statement::DoBlock(block);
+        assert_eq!(
+            do_block.exec(&mut env),
+            Ok(Some(vec![LuaValue::new(LuaVal::LuaNum(
+                10i64.to_be_bytes()
+            ))]))
+        );
+        assert_eq!(
+            env.get("a"),
+            Some(&lua_integer(10))
+        );
+    }
+
+    #[test]
+    fn test_exec_stat_while_break() {
+        let mut env = Env::new();
+        let condition = Expression::True;
+        let block = Block {
+            statements: vec![
+                Statement::Assignment((
+                    vec![Var::NameVar("a".to_string())],
+                    vec![Expression::Numeral(Numeral::Integer(10))],
+                )),
+                Statement::Break,
+            ],
+            return_stat: None,
+        };
+        let while_stat = Statement::While((condition, block));
+        assert_eq!(while_stat.exec(&mut env), Ok(Some(vec![])));
+        assert_eq!(
+            env.get("a"),
+            Some(&lua_integer(10))
+        );
+    }
+
+    #[test]
+    fn test_exec_stat_while_return() {
+        let mut env = Env::new();
+        let condition = Expression::True;
+        let block = Block {
+            statements: vec![Statement::Assignment((
+                vec![Var::NameVar("a".to_string())],
+                vec![Expression::Numeral(Numeral::Integer(10))],
+            ))],
+            return_stat: Some(vec![var_exp("a")]),
+        };
+        let while_stat = Statement::While((condition, block));
+        assert_eq!(while_stat.exec(&mut env), Ok(Some(vec![lua_integer(10)])));
+        assert_eq!(env.get("a"), Some(&lua_integer(10)));
+    }
+
+    #[test]
+    fn test_exec_stat_while() {
+        // TODO: increment few times when binary expression is implemented
+        // let mut env = Env::new();
+        // let condition = Expression::False;
+        // let block = Block {
+        //     statements: vec![Statement::Assignment((
+        //         vec![Var::NameVar("a".to_string())],
+        //         vec![Expression::Numeral(Numeral::Integer(10))],
+        //     ))],
+        //     return_stat: None,
+        // };
     }
 }
