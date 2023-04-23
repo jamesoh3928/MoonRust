89d03a22fad01959ef4b1d499d70ad6a822532d2
matthewdellaneve <mjdellaneve@gmail.com>
2023-04-23 13:44:48 -0400

    Implemented Display for AST

--- moonrust/src/ast.rs
+++ moonrust/src/ast.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Statement {
     Assignment((Vec<Var>, Vec<Expression>, bool)), // bool flag: true if local assn, false otherwise
@@ -14,6 +16,101 @@ pub enum Statement {
     Semicolon,
 }
 
+impl Display for Statement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Statement::Assignment((vars, exps, local)) => {
+                if *local {
+                    write!(f, "local ")?;
+                }
+                format_list(vars, f, false)?;
+                write!(f, " = ")?;
+                format_list(exps, f, false)?;
+            }
+            Statement::FunctionCall(fncall) => fncall.fmt(f)?,
+            Statement::Break => write!(f, "break")?,
+            Statement::DoBlock(block) => {
+                writeln!(f, "do")?;
+                block.fmt(f)?;
+                write!(f, "end")?;
+            }
+            Statement::While((exp, block)) => {
+                write!(f, "while ")?;
+                exp.fmt(f)?;
+                writeln!(f, " do")?;
+                block.fmt(f)?;
+                write!(f, "end")?;
+            }
+            Statement::Repeat((block, exp)) => {
+                writeln!(f, "repeat")?;
+                block.fmt(f)?;
+                write!(f, "until ")?;
+                exp.fmt(f)?;
+            }
+            Statement::If((cond, then_block, elseifs, else_block)) => {
+                write!(f, "if ")?;
+                cond.fmt(f)?;
+                writeln!(f, " then")?;
+                then_block.fmt(f)?;
+                elseifs.iter().try_for_each(|elseif| {
+                    write!(f, "elseif ")?;
+                    elseif.0.fmt(f)?;
+                    writeln!(f, " then")?;
+                    elseif.1.fmt(f)
+                })?;
+                if let Some(elseb) = else_block {
+                    writeln!(f, "else")?;
+                    elseb.fmt(f)?;
+                }
+            }
+            Statement::ForNum((name, exp1, exp2, maybe_exp3, block)) => {
+                write!(f, "for ")?;
+                name.fmt(f)?;
+                write!(f, " = ")?;
+                exp1.fmt(f)?;
+                write!(f, ", ")?;
+                exp2.fmt(f)?;
+                if let Some(exp3) = maybe_exp3 {
+                    write!(f, ", ")?;
+                    exp3.fmt(f)?;
+                }
+                writeln!(f, " do")?;
+                block.fmt(f)?;
+                write!(f, "end")?;
+            }
+            Statement::ForGeneric((names, exps, block)) => {
+                write!(f, "for ")?;
+                format_list(names, f, false)?;
+                write!(f, " in ")?;
+                format_list(exps, f, false)?;
+                writeln!(f, " do")?;
+                block.fmt(f)?;
+                write!(f, "end")?;
+            }
+            Statement::FunctionDecl((name, parlist, block)) => {
+                write!(f, "function ")?;
+                name.fmt(f)?;
+                write!(f, "(")?;
+                parlist.fmt(f)?;
+                writeln!(f, ")")?;
+                block.fmt(f)?;
+                write!(f, "end")?;
+            }
+            Statement::LocalFuncDecl((name, parlist, block)) => {
+                write!(f, "local function ")?;
+                name.fmt(f)?;
+                write!(f, "(")?;
+                parlist.fmt(f)?;
+                writeln!(f, ")")?;
+                block.fmt(f)?;
+                write!(f, "end")?;
+            }
+            Statement::Semicolon => write!(f, ";")?,
+        }
+        writeln!(f)
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expression {
     Nil,
@@ -27,6 +124,46 @@ pub enum Expression {
     TableConstructor(Vec<Field>),
     BinaryOp((Box<Expression>, BinOp, Box<Expression>)),
     UnaryOp((UnOp, Box<Expression>)),
+}
+
+impl Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Nil => write!(f, "nil"),
+            Expression::False => write!(f, "false"),
+            Expression::True => write!(f, "true"),
+            Expression::Numeral(numeral) => numeral.fmt(f),
+            Expression::LiteralString(string) => {
+                write!(f, "\"")?;
+                string.fmt(f)?;
+                write!(f, "\"")
+            }
+            Expression::DotDotDot => write!(f, "..."),
+            Expression::FunctionDef((parlist, block)) => {
+                write!(f, "function")?;
+                write!(f, "(")?;
+                parlist.fmt(f)?;
+                writeln!(f, ")")?;
+                block.fmt(f)?;
+                writeln!(f, "end")
+            }
+            Expression::PrefixExp(pexp) => pexp.fmt(f),
+            Expression::TableConstructor(fields) => {
+                writeln!(f, "{{")?;
+                format_list(&fields, f, true)?;
+                writeln!(f, "}}")
+            }
+            Expression::BinaryOp((exp1, binop, exp2)) => {
+                exp1.fmt(f)?;
+                binop.fmt(f)?;
+                exp2.fmt(f)
+            }
+            Expression::UnaryOp((unop, exp)) => {
+                unop.fmt(f)?;
+                exp.fmt(f)
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -54,12 +191,51 @@ pub enum BinOp {
     LogicalOr,
 }
 
+impl Display for BinOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BinOp::Add => write!(f, " + "),
+            BinOp::Sub => write!(f, " - "),
+            BinOp::Mult => write!(f, " * "),
+            BinOp::Div => write!(f, " / "),
+            BinOp::IntegerDiv => write!(f, " // "),
+            BinOp::Pow => write!(f, " ^ "),
+            BinOp::Mod => write!(f, " % "),
+            BinOp::BitAnd => write!(f, " & "),
+            BinOp::BitXor => write!(f, " ~ "),
+            BinOp::BitOr => write!(f, " | "),
+            BinOp::ShiftRight => write!(f, " >> "),
+            BinOp::ShiftLeft => write!(f, " << "),
+            BinOp::Concat => write!(f, " .. "),
+            BinOp::LessThan => write!(f, " < "),
+            BinOp::LessEq => write!(f, " <= "),
+            BinOp::GreaterThan => write!(f, " > "),
+            BinOp::GreaterEq => write!(f, " >= "),
+            BinOp::Equal => write!(f, " == "),
+            BinOp::NotEqual => write!(f, " ~= "),
+            BinOp::LogicalAnd => write!(f, " and "),
+            BinOp::LogicalOr => write!(f, " or "),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum UnOp {
     Negate,
     LogicalNot,
     Length,
     BitNot,
+}
+
+impl Display for UnOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Negate => write!(f, "-"),
+            Self::LogicalNot => write!(f, "not"),
+            Self::Length => write!(f, "#"),
+            Self::BitNot => write!(f, "~"),
+        }
+    }
 }
 
 // In parsing, we store numeral in i64 and f64, but in interpreting, we store them as [u8; 8]
@@ -69,6 +245,15 @@ pub enum Numeral {
     Float(f64),
 }
 
+impl Display for Numeral {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Integer(i) => i.fmt(f),
+            Self::Float(fl) => fl.fmt(f),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum PrefixExp {
     Var(Var),
@@ -76,10 +261,37 @@ pub enum PrefixExp {
     Exp(Expression),
 }
 
+impl Display for PrefixExp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Var(var) => var.fmt(f),
+            Self::FunctionCall(fncall) => fncall.fmt(f),
+            Self::Exp(exp) => exp.fmt(f),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum FunctionCall {
     Standard((Box<PrefixExp>, Args)),
     Method((Box<PrefixExp>, String, Args)),
+}
+
+impl Display for FunctionCall {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Standard((pexp, args)) => {
+                pexp.fmt(f)?;
+                args.fmt(f)
+            }
+            Self::Method((pexp, name, args)) => {
+                pexp.fmt(f)?;
+                write!(f, ":")?;
+                name.fmt(f)?;
+                args.fmt(f)
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -89,14 +301,71 @@ pub enum Args {
     LiteralString(String),
 }
 
+impl Display for Args {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::ExpList(exps) => {
+                write!(f, "(")?;
+                format_list(&exps, f, false)?;
+                write!(f, ")")
+            }
+            Self::TableConstructor(fields) => {
+                writeln!(f, "{{")?;
+                format_list(&fields, f, true)?;
+                writeln!(f, "}}")
+            }
+            Self::LiteralString(string) => {
+                write!(f, "\"")?;
+                string.fmt(f)?;
+                write!(f, "\"")
+            }
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct ParList(pub Vec<String>, pub bool); // boolean flag is true if there are varargs
+
+impl Display for ParList {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.0.is_empty() {
+            write!(f, "...")
+        } else {
+            format_list(&self.0, f, false)?;
+
+            if self.1 {
+                write!(f, ", ...")
+            } else {
+                Ok(())
+            }
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Field {
     BracketedAssign((Expression, Expression)),
     NameAssign((String, Expression)),
     UnnamedAssign(Expression),
+}
+
+impl Display for Field {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::BracketedAssign((exp1, exp2)) => {
+                write!(f, "[")?;
+                exp1.fmt(f)?;
+                write!(f, "] = ")?;
+                exp2.fmt(f)
+            }
+            Self::NameAssign((name, exp)) => {
+                name.fmt(f)?;
+                write!(f, " = ")?;
+                exp.fmt(f)
+            }
+            Self::UnnamedAssign(exp) => exp.fmt(f),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -106,11 +375,131 @@ pub enum Var {
     DotVar((Box<PrefixExp>, String)),
 }
 
+impl Display for Var {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NameVar(name) => name.fmt(f),
+            Self::BracketVar((pexp, exp)) => {
+                pexp.fmt(f)?;
+                write!(f, "[")?;
+                exp.fmt(f)?;
+                write!(f, "]")
+            }
+            Self::DotVar((pexp, name)) => {
+                pexp.fmt(f)?;
+                write!(f, ".")?;
+                name.fmt(f)
+            }
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct Block {
     pub statements: Vec<Statement>,
     pub return_stat: Option<Vec<Expression>>,
 }
 
+impl Display for Block {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.statements.iter().try_for_each(|statement| {
+            write!(f, "    ")?;
+            statement.fmt(f)
+        })?;
+
+        if let Some(ret) = &self.return_stat {
+            write!(f, "    return ")?;
+            ret.iter().try_for_each(|exp| exp.fmt(f))
+        } else {
+            Ok(())
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct AST(pub Block);
+
+impl Display for AST {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0
+            .statements
+            .iter()
+            .try_for_each(|statement| statement.fmt(f))?;
+
+        if let Some(ret) = &self.0.return_stat {
+            ret.iter().try_for_each(|exp| exp.fmt(f))?;
+        }
+
+        writeln!(f)
+    }
+}
+
+fn format_list(
+    elements: &Vec<impl Display>,
+    f: &mut std::fmt::Formatter<'_>,
+    lines: bool,
+) -> std::fmt::Result {
+    let mut el_iter = elements.iter().peekable();
+    while let Some(element) = el_iter.next() {
+        element.fmt(f)?;
+        if el_iter.peek().is_some() {
+            if lines {
+                writeln!(f, ", ")?;
+            } else {
+                write!(f, ", ")?;
+            }
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn foobar() {
+    let thing = Statement::While((
+        Expression::BinaryOp((
+            Box::new(Expression::PrefixExp(Box::new(PrefixExp::Var(
+                Var::NameVar(String::from("i")),
+            )))),
+            BinOp::LessEq,
+            Box::new(Expression::PrefixExp(Box::new(PrefixExp::Var(
+                Var::NameVar(String::from("x")),
+            )))),
+        )),
+        Block {
+            statements: vec![
+                Statement::Assignment((
+                    vec![Var::NameVar(String::from("x"))],
+                    vec![Expression::BinaryOp((
+                        Box::new(Expression::PrefixExp(Box::new(PrefixExp::Var(
+                            Var::NameVar(String::from("i")),
+                        )))),
+                        BinOp::Mult,
+                        Box::new(Expression::Numeral(Numeral::Integer(2))),
+                    ))],
+                    true,
+                )),
+                Statement::FunctionCall(FunctionCall::Standard((
+                    Box::new(PrefixExp::Var(Var::NameVar(String::from("print")))),
+                    Args::ExpList(vec![Expression::PrefixExp(Box::new(PrefixExp::Var(
+                        Var::NameVar(String::from("x")),
+                    )))]),
+                ))),
+                Statement::Assignment((
+                    vec![Var::NameVar(String::from("i"))],
+                    vec![Expression::BinaryOp((
+                        Box::new(Expression::PrefixExp(Box::new(PrefixExp::Var(
+                            Var::NameVar(String::from("i")),
+                        )))),
+                        BinOp::Add,
+                        Box::new(Expression::Numeral(Numeral::Integer(1))),
+                    ))],
+                    false,
+                )),
+            ],
+            return_stat: None,
+        },
+    ));
+    println!("{}", thing);
+
+    assert!(false);
+}
