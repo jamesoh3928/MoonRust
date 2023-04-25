ccbf3fc270b9b93285bc703634708e5b1489cb6b
James Oh <jo9347@cs.rit.edu>
2023-04-25 16:27:44 -0400

    Add some lua test files

---
+++ moonrust/assets/function_env.lua
@@ -0,0 +1,8 @@
+do
+    local x = 1
+    function f()
+        return x
+    end
+end
+
+print(f())
---
+++ moonrust/assets/function_env2.lua
@@ -0,0 +1,11 @@
+do
+    do 
+      function f()
+        return x
+      end
+    end
+  end
+  
+  local x = 2
+  print(f())
+  print(x)
