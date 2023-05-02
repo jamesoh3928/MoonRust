7c8f1757632db9407625cd6feaf95fb336c6419d
James Oh <jo9347@cs.rit.edu>
2023-05-02 14:58:23 -0400

    Add test cases for table

--- moonrust/assets/table1.lua
+++ moonrust/assets/table1.lua
@@ -1,9 +1,11 @@
 local t = {}
 t[1] = "x"
 t[2] = "y"
-t.x = 1
 t[30] = 23
 t[4] = 45
 a = t
-print(t[1], t[2], t.x, t[30], t[4])
-print(a[1], a[2], a.x, a[30], a[4])
+print(t[1], t[2], t[30], t[4])
+print(a[1], a[2], a[30], a[4])
+a[1] = "z"
+print(t[1], t[2], t[30], t[4])
+print(a[1], a[2], a[30], a[4])
